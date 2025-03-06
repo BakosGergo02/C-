0c3d13188b478fc6565866b296830c9815dc9982
Bakos Gergő <99805027+BakosGergo02@users.noreply.github.com>
2025-03-06 22:35:41 +0100

    Add files via upload

---
+++ labor/13.ora/Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Dictionary<string, int>  dict = new Dictionary<string, int>();
+            
+            var dict2 = new Dictionary<string, int>();
+            Dictionary<string, int> dict3 = new();
+
+            dict2.Add("a", 1);
+            dict2.Add("b", 2);
+            dict2["c"] = 5;
+            dict2["d"] = 6;
+
+            Dictionary<string, int> dict4 = new Dictionary<string, int>
+            {
+                { "a", 5 },
+                { "b", 6 }
+            };
+
+            Dictionary<string, int> dict6 = new()
+            {
+                ["c"] = 5,
+                ["d"] = 7,
+            };
+
+            Console.WriteLine(dict6["c"]);
+
+            // kivetelt dob, ha kulcs mér létezik a dic-ben
+            // dict.add("c",3 );
+
+            dict6.TryAdd("a", 1);
+        }
+    }
+}
