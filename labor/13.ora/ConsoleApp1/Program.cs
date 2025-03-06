3779bb06e3e4232ef0ba73eb33c2674eb6ba7fb4
Bakos Gergő <99805027+BakosGergo02@users.noreply.github.com>
2025-03-06 22:36:45 +0100

    Add files via upload

---
+++ labor/13.ora/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MultidimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //jagged array <==> tömbök tömbje
+            int[][] mat = new int[][]
+            {
+                new int[] {1,2,3},
+                new int[] {1,2},
+                new int[] {1}
+
+            };
+
+            foreach (int[] innerArray in mat)
+            {
+                foreach (int num in innerArray)
+                {
+                    Console.WriteLine(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //többdimenzios tömb
+
+            int[,] mat2 = new int[,] {
+                { 1, 2 },
+                { 3, 4},
+                { 5, 6}
+            };
+
+            mat2.GetLength(0); // ?
+        }
+    }
+}
